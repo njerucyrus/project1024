850588239fee1da749bee7830369d869d71b63cd
Njeru Cyrus <njerucyrus5@gmail.com>
2020-09-28 02:39:45 +0300

    added condoms faqs

---
+++ app/src/main/java/com/apps/project1024/ui/resource_center/CondomsActivity.java
@@ -0,0 +1,16 @@
+package com.apps.project1024.ui.resource_center;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.apps.project1024.R;
+
+public class CondomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condoms);
+    }
+}
