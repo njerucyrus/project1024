87a2e91a6b94e7416edbabbc5fa022faea0a4104
mwaphinju <phlpnjuguna@gmail.com>
2020-10-11 12:31:10 +0300

    User profile added

---
+++ app/src/main/java/com/apps/project1024/ui/auth/UserProfile.java
@@ -0,0 +1,16 @@
+package com.apps.project1024.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.apps.project1024.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
